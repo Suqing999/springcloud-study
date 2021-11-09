52dee3b9831fb023a1cc87db094271b589a63424
Suki <1162457366@qq.com>
2021-11-09 15:59:30 +0800

    second

---
+++ springcloud-config-client-3355/src/main/java/com/baidu/springcloud/Config_Client_3355.java
@@ -0,0 +1,12 @@
+package com.baidu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class Config_Client_3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(Config_Client_3355.class, args);
+    }
+}
