4dc9692c5b65c5a9493b31a7cb326ba9b1d56a97
Suki <1162457366@qq.com>
2021-11-09 16:27:59 +0800

    third

---
+++ provider-dept-config/src/main/java/com/baidu/springcloud/DeptProvider_8001_config.java
@@ -0,0 +1,19 @@
+package com.baidu.springcloud;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+//启动类
+@SpringBootApplication
+@EnableEurekaClient
+public class DeptProvider_8001_config {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptProvider_8001_config.class, args);
+    }
+
+}
---
+++ provider-dept-config/src/main/java/com/baidu/springcloud/controller/DeptController.java
@@ -0,0 +1,47 @@
+package com.baidu.springcloud.controller;
+
+import com.baidu.springcloud.pojo.Dept;
+import com.baidu.springcloud.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class DeptController {
+    //提供restful服务
+    @Autowired
+    DeptService deptService;
+    //获取配置信息
+    @Autowired
+    DiscoveryClient discoveryClient;
+    @PostMapping("/dept/add")
+    public boolean addDept(@RequestBody Dept dept){
+        return deptService.addDept(dept);
+    }
+    @GetMapping("/dept/get/{id}")
+    public Dept queryByid(@PathVariable("id") Long id){
+        return deptService.queryDeptById(id);
+    }
+    @GetMapping("/dept/list")
+    public List<Dept> queryAll(){
+        return deptService.queryDept();
+    }
+    //注册进来的微服务获取一些消息
+    //个人用不到
+    // 协同需要
+    @RequestMapping("/dept/discovery ")
+    public Object discovery(){
+        List<String> services = discoveryClient.getServices();
+        System.out.println("discover=>"+services);
+        //得到一个具体微服务信息
+        List<ServiceInstance> instances = discoveryClient.getInstances("SPRINGCLOUD-PROVIDER-DEPT");//通过具体微服务名字拿到信息
+        for (ServiceInstance instance : instances) {
+            System.out.println(instance); //服务的端口，主机，等等内容
+        }
+        return this.discoveryClient;
+    }
+}
